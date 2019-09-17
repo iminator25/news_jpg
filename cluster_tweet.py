9c4d0297754828b9a5a52bef5b309bf738dd6c0e
Caleb Carithers <calebcarithers@me.com>
2019-09-17 17:57:26 -0400

    Initial commit

---
+++ cluster_tweet.py
@@ -0,0 +1,93 @@
+import sys
+from PIL import Image
+import headline_image
+import tweepy
+import os
+import shutil
+import keys
+
+class createCluster():
+    name_number = 0
+    new_height = 350
+
+    def __init__(self, urls, r, g, b):
+        self.urls = urls
+        self.background = (r, g, b)
+        self.images = list(map(Image.open, self.urls))
+        self.img_length = len(self.images)
+        self.count = 0
+        self.imgs_per_row = 3
+
+    def resizeImages(self):
+        index = 0
+        for im in self.images:
+            new_width = (createCluster.new_height*im.width)//im.height
+            im = im.resize((new_width,createCluster.new_height), Image.ANTIALIAS)
+            im.save(self.urls[index], "JPEG")
+            self.images = list(map(Image.open, self.urls))
+            index += 1
+        print(self.img_length)
+
+    def createCluster(self):
+        width_per_row = []
+        temp = 0
+
+        for im in self.images:
+            temp += im.width
+            self.count += 1
+            if self.count == self.imgs_per_row:
+                width_per_row.append(temp)
+                temp = 0
+                self.count = 0
+        self.count = 0
+
+        total_width = max(width_per_row)
+        max_height = (int(self.img_length/self.imgs_per_row) + (self.img_length % self.imgs_per_row)) * createCluster.new_height
+
+        new_im = Image.new('RGB', (total_width, max_height), self.background)
+        x_offset = 0
+        y_coord = 0
+
+        for im in self.images:
+            if self.count > self.imgs_per_row - 1:
+                y_coord += createCluster.new_height
+                x_offset = 0
+                self.count = 0
+            new_im.paste(im, (x_offset,y_coord))
+            x_offset += im.size[0]
+            im.close()
+            self.count += 1
+
+        self.path_to_image = '%d.jpg' % createCluster.name_number
+        new_im.save(self.path_to_image)
+        createCluster.name_number += 1
+        return self.path_to_image
+
+class TweetIt:
+
+    def __init__(self, img, headline):
+        self.img = img
+        self.headline = headline
+
+    def authorize(self):
+        auth = tweepy.OAuthHandler(keys.twitCAPI, keys.twitCAPISecret)
+        auth.set_access_token(keys.twitAccess, keys.twitAccessSecret)
+        self.api = tweepy.API(auth)
+
+    def tweet(self):
+        self.api.update_with_media(self.img, status=self.headline)
+
+
+
+for entry in headline_image.masterList:
+    print(entry['urls'])
+    newCluster = createCluster(entry['urls'], 0, 0, 0)
+    newCluster.resizeImages()
+    path = newCluster.createCluster()
+
+    newTweet = TweetIt(path, "\"" + entry['title'] + "\"" + " -" + entry['source'] + " " + "(" + entry['source_url'] + ")")
+    newTweet.authorize()
+    newTweet.tweet()
+    os.remove(path)
+    shutil.rmtree('/Users/calebcarithers/Desktop/news_jpg/static/Resources/Images')
+
---
+++ headline_image.py
@@ -0,0 +1,55 @@
+import requests
+import datetime
+from google_images_download import google_images_download
+from random import randint
+import string
+import keys
+
+#Get the current date
+now = datetime.datetime.now().date()
+
+# Data structure for headlines || masterList = [{'source':'news source', 'title':'headline title',
+# 'source_url':'the sauce', 'urls': [all downloaded image paths]}]
+masterList = []
+
+# Get headlines
+url = ('https://newsapi.org/v2/top-headlines?'
+       'country=us&pageSize=1&'
+       'apiKey=' + keys.newsAPIkey)
+response = requests.get(url)
+
+# Add headlines to masterList in structure specified on lines 12 & 13
+for thing in response.json()['articles']:
+    headLine = thing['title']
+    masterList.append({'source':thing['source']['name'], 'title':str(headLine[:headLine.rfind("-")]), 'source_url':thing['url']})
+
+# Get images for each word in headline
+response = google_images_download.googleimagesdownload()
+
+# Parameters & initializer for downloading
+imageLimit = 3
+imageFileNumber = 0
+counter = 0
+
+# For each dictionary in the list add list of downloaded image paths
+for item in masterList:
+    urlList = []
+    # Iterate through headline without punctuation
+    for word in item['title'].translate(str.maketrans('', '', string.punctuation)).split():
+        arguments = {"keywords": word, "limit": imageLimit,
+                     "print_urls": False,
+                     "output_directory": "static/Resources/Images/%s/%d" % (now, imageFileNumber),
+                     'format': 'jpg'}
+        paths = response.download(arguments)  # store paths from response
+
+        try:
+            randToChoose = randint(0, imageLimit-1)  # choose random image as the one to add to url list
+            urlList.append(paths[0][word][randToChoose])
+        except IndexError:  # if image does not download try again
+            paths = response.download(arguments)
+            randToChoose = randint(0, imageLimit - 1)
+            urlList.append(paths[0][word][randToChoose])
+
+    masterList[counter]['urls'] = urlList
+    imageFileNumber += 1
+    counter += 1
